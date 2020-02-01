3299bc8e87789d8766a42570d6eebd06ba30dff5
Juanma <altunajuan@gmail.com>
2020-02-01 01:30:45 -0300

    Created wall status scripts

---
+++ Assets/Scripts/WallStatus.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum Status
+{
+    untouched,
+    damaged,
+    broken,
+    destroyed
+}
+
+public class WallStatus : MonoBehaviour
+{
+    Status status;
+    
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("SnowBall"))
+        {
+            Repair();
+            Destroy(collision.gameObject);
+        }
+    }
+
+
+    public void Damage()
+    {
+        status++;
+        Debug.Log(status);
+        if(status == Status.destroyed)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Repair()
+    {
+        if(status != 0 )
+        {
+            status--;
+        }        
+    }
+}
