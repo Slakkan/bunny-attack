b4a90b1164ab768d8d9021804bdc038601f0c41e
Juanma <altunajuan@gmail.com>
2020-02-01 01:48:54 -0300

    Created rabbit attack

---
+++ Assets/Scripts/RabbitAttack.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RabbitAttack : MonoBehaviour
+{
+
+    public float atackInterval;
+    Transform currentTarget;
+
+    
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Wall"))
+        {
+            InvokeRepeating("Attack", atackInterval, atackInterval);
+            currentTarget = collision.transform;
+        }
+    }
+
+    void Attack()
+    {
+        if(currentTarget != null)
+        {
+            currentTarget.GetComponent<WallStatus>().Damage();
+        }        
+    }
+}
