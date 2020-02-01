6f4324c4275aca4a0b1e39f6d3024d0978cf2d81
Slakkan <lnpastinante@gmail.com>
2020-02-01 09:07:52 -0300

    Added snowball generation scripts, fixed minor bugs

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public GameObject snowball;
+    public Vector3 snowballOffset = new Vector3(+0.5f, 0, 0);
+    float chargeStart;
+    float chargeTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCharging();
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            StopCharging();
+        }
+    }
+
+    void StartCharging()
+    {
+        chargeStart = Time.time;
+    }
+
+    void StopCharging()
+    {
+        chargeTime = Time.time - chargeStart;
+        Shoot();
+    }
+
+    void Shoot()
+    {
+        GameObject newBall = Instantiate<GameObject>(snowball, transform.position - snowballOffset, snowball.transform.rotation);
+        SnowballMovement movement = newBall.GetComponent<SnowballMovement>();
+        movement.impulseForce = Mathf.Min(8, 5 + chargeTime);
+    }
+}
